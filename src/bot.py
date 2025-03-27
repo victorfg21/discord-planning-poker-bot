87045fa608ddbb0d9eef1ed0776d672d7f7fc5a7
Victor Fernando Ferreira Gomes <victorgbpk@gmail.com>
2025-03-27 18:29:39 -0300

    Refactor Planning Poker bot: migrate to new structure and add dotenv support

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,11 @@
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+import os
 from discord.ui import View, Button, Modal, TextInput
 
-TOKEN = "MY_TOKEN"
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
